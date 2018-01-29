bd24a51f52bec1a6e69752fd3cfea6e864f76d05
Nico Schlömer <nico.schloemer@gmail.com>
2018-01-29 20:58:32 +0100

    fix typo

--- cplot/__about__.py
+++ cplot/__about__.py
@@ -2,9 +2,9 @@
 #
 
 __author__ = u'Nico Schlömer'
-__author__email__ = 'nico.schloemer@gmail.com'
+__author_email__ = 'nico.schloemer@gmail.com'
 __copyright__ = \
-    u'Copyright (c) 2018 {} <{}>'.format(__author__, __author__email__)
+    u'Copyright (c) 2018 {} <{}>'.format(__author__, __author_email__)
 __credits__ = []
 __license__ = 'License :: OSI Approved :: MIT License'
 __version__ = '0.0.1'
