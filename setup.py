a0391f22f57df2cf678fb4243dd5e2b397166548
Nico Schlömer <nico.schloemer@gmail.com>
2018-09-18 11:18:32 +0200

    more updates for setup.py

--- setup.py
+++ setup.py
@@ -12,35 +12,28 @@ with open(os.path.join(base_dir, "cplot", "__about__.py"), "rb") as f:
 
 
 def read(fname):
-    try:
-        content = codecs.open(
-            os.path.join(os.path.dirname(__file__), fname), encoding="utf-8"
-        ).read()
-    except Exception:
-        content = ""
-    return content
+    return codecs.open(os.path.join(base_dir, fname), encoding="utf-8").read()
 
 
 setup(
     name="cplot",
+    packages=find_packages(),
     version=about["__version__"],
     author=about["__author__"],
     author_email=about["__author_email__"],
-    packages=find_packages(),
-    description="plotting tools for complex-valued functions",
-    long_description=read("README.rst"),
+    description="Plotting tools for complex-valued functions",
+    long_description=read("README.md"),
     url="https://github.com/nschloe/cplot",
-    download_url="https://pypi.python.org/pypi/cplot",
     license=about["__license__"],
     platforms="any",
-    install_requires=["colorio", "matplotlib", "numpy", "pipdate"],
+    install_requires=["colorio", "matplotlib", "numpy"],
+    long_description_content_type="text/markdown",
     classifiers=[
         about["__status__"],
         about["__license__"],
         "Intended Audience :: Science/Research",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
         "Topic :: Scientific/Engineering",
     ],
