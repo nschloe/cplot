bba0408e52bc3dcbc3231f9b8dd21d982a612d83
Nico Schlömer <nico.schloemer@gmail.com>
2018-09-18 11:21:37 +0200

    version bump

--- cplot/__about__.py
+++ cplot/__about__.py
@@ -5,6 +5,6 @@ __author__ = u"Nico Schlömer"
 __author_email__ = "nico.schloemer@gmail.com"
 __copyright__ = u"Copyright (c) 2018 {} <{}>".format(__author__, __author_email__)
 __license__ = "License :: OSI Approved :: MIT License"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __maintainer__ = "Nico Schlömer"
 __status__ = "Development Status :: 1 - Planning"
