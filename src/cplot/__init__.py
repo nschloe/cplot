ff53ebc5a8727455c45073bef6102f3bff69c4af
Nico Schlömer <nico.schloemer@gmail.com>
2021-07-01 13:59:01 +0200

    remove __about__

--- src/cplot/__init__.py
+++ src/cplot/__init__.py
@@ -1,12 +1,9 @@
-from .__about__ import __version__
 from ._colors import get_srgb1, tripcolor
 from ._main import Plot, plot, savefig, show, show_colors, show_contours
 from .benchmark import show_kovesi_test_image, show_test_function
 from .create import create_colormap, find_max_srgb_radius, show_circular, show_linear
 
 __all__ = [
-    "__version__",
-    #
     "show_test_function",
     "show_kovesi_test_image",
     "show_linear",
