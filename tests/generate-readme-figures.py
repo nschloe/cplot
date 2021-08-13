b70475a9218cc5e81054b1237acc90f75951ad30
Nico Schlömer <nico.schloemer@gmail.com>
2021-08-13 17:49:55 +0200

    test fix

--- tests/generate-readme-figures.py
+++ tests/generate-readme-figures.py
@@ -64,53 +64,62 @@ def siam(z):
 
 n = 400
 
-cplot.savefig("z1.png", lambda z: z ** 1, (-2, +2), (-2, +2), n)
-cplot.savefig("z2.png", lambda z: z ** 2, (-2, +2), (-2, +2), n)
-cplot.savefig("z3.png", lambda z: z ** 3, (-2, +2), (-2, +2), n)
+args = [
+    ("sinz3z.png", lambda z: np.sin(z ** 3) / z, (-2, 2), (-2, 2)),
+    #
+    ("z1.png", lambda z: z ** 1, (-2, +2), (-2, +2)),
+    ("z2.png", lambda z: z ** 2, (-2, +2), (-2, +2)),
+    ("z3.png", lambda z: z ** 3, (-2, +2), (-2, +2)),
+    #
+    ("1z.png", lambda z: 1 / z, (-2, +2), (-2, +2)),
+    ("z-absz.png", lambda z: z / abs(z), (-2, +2), (-2, +2)),
+    ("z+1-z-1.png", lambda z: (z + 1) / (z - 1), (-5, +5), (-5, +5)),
+    #
+    ("zz.png", lambda z: z ** z, (-3, +3), (-3, +3)),
+    ("1zz.png", lambda z: (1 / z) ** z, (-3, +3), (-3, +3)),
+    ("z1z.png", lambda z: z ** (1 / z), (-3, +3), (-3, +3)),
+    #
+    ("root2.png", np.sqrt, (-2, +2), (-2, +2)),
+    ("root3.png", lambda x: x ** (1 / 3), (-2, +2), (-2, +2)),
+    ("root4.png", lambda x: x ** 0.25, (-2, +2), (-2, +2)),
+    #
+    ("log.png", np.log, (-2, +2), (-2, +2)),
+    ("exp.png", np.exp, (-2, +2), (-2, +2)),
+    ("exp1z.png", lambda z: np.exp(1 / z), (-1, +1), (-1, +1)),
+    #
+    ("sin.png", np.sin, (-5, +5), (-5, +5)),
+    ("cos.png", np.cos, (-5, +5), (-5, +5)),
+    ("tan.png", np.tan, (-5, +5), (-5, +5)),
+    #
+    ("sinh.png", np.sinh, (-5, +5), (-5, +5)),
+    ("cosh.png", np.cosh, (-5, +5), (-5, +5)),
+    ("tanh.png", np.tanh, (-5, +5), (-5, +5)),
+    #
+    ("arcsin.png", np.arcsin, (-2, +2), (-2, +2)),
+    ("arccos.png", np.arccos, (-2, +2), (-2, +2)),
+    ("arctan.png", np.arctan, (-2, +2), (-2, +2)),
+    #
+    ("sinz-z.png", lambda z: np.sin(z) / z, (-7, +7), (-7, +7)),
+    ("cosz-z.png", lambda z: np.cos(z) / z, (-7, +7), (-7, +7)),
+    ("tanz-z.png", lambda z: np.tan(z) / z, (-7, +7), (-7, +7)),
+    #
+    ("gamma.png", scipy.special.gamma, (-5, +5), (-5, +5)),
+    ("digamma.png", scipy.special.digamma, (-5, +5), (-5, +5)),
+    ("zeta.png", riemann_zeta, (-30, +30), (-30, +30)),
+    #
+    ("riemann-xi.png", riemann_xi, (-20, +20), (-20, +20)),
+    ("riemann-siegel-z.png", riemann_siegel_z, (-20, +20), (-20, +20)),
+    ("riemann-siegel-theta.png", riemann_siegel_theta, (-20, +20), (-20, +20)),
+]
+for a in args:
+    cplot.plot(*a[1:], n=n, colorbars=False)
+    plt.savefig(a[0], transparent=True, bbox_inches="tight")
+    plt.close()
 
-cplot.savefig("1z.png", lambda z: 1 / z, (-2, +2), (-2, +2), n)
-cplot.savefig("z-absz.png", lambda z: z / abs(z), (-2, +2), (-2, +2), n)
-cplot.savefig("z+1-z-1.png", lambda z: (z + 1) / (z - 1), (-5, +5), (-5, +5), n)
 
-cplot.savefig("zz.png", lambda z: z ** z, (-3, +3), (-3, +3), n)
-cplot.savefig("1zz.png", lambda z: (1 / z) ** z, (-3, +3), (-3, +3), n)
-cplot.savefig("z1z.png", lambda z: z ** (1 / z), (-3, +3), (-3, +3), n)
-
-cplot.savefig("root2.png", np.sqrt, (-2, +2), (-2, +2), n)
-cplot.savefig("root3.png", lambda x: x ** (1 / 3), (-2, +2), (-2, +2), n)
-cplot.savefig("root4.png", lambda x: x ** 0.25, (-2, +2), (-2, +2), n)
-
-cplot.savefig("log.png", np.log, (-2, +2), (-2, +2), n)
-cplot.savefig("exp.png", np.exp, (-2, +2), (-2, +2), n)
-cplot.savefig("exp1z.png", lambda z: np.exp(1 / z), (-1, +1), (-1, +1), n)
-
-cplot.savefig("sin.png", np.sin, (-5, +5), (-5, +5), n)
-cplot.savefig("cos.png", np.cos, (-5, +5), (-5, +5), n)
-cplot.savefig("tan.png", np.tan, (-5, +5), (-5, +5), n)
-
-cplot.savefig("sinh.png", np.sinh, (-5, +5), (-5, +5), n)
-cplot.savefig("cosh.png", np.cosh, (-5, +5), (-5, +5), n)
-cplot.savefig("tanh.png", np.tanh, (-5, +5), (-5, +5), n)
-
-cplot.savefig("arcsin.png", np.arcsin, (-2, +2), (-2, +2), n)
-cplot.savefig("arccos.png", np.arccos, (-2, +2), (-2, +2), n)
-cplot.savefig("arctan.png", np.arctan, (-2, +2), (-2, +2), n)
-
-cplot.savefig("sinz-z.png", lambda z: np.sin(z) / z, (-7, +7), (-7, +7), n)
-cplot.savefig("cosz-z.png", lambda z: np.cos(z) / z, (-7, +7), (-7, +7), n)
-cplot.savefig("tanz-z.png", lambda z: np.tan(z) / z, (-7, +7), (-7, +7), n)
-
-cplot.savefig("gamma.png", scipy.special.gamma, (-5, +5), (-5, +5), n)
-cplot.savefig("digamma.png", scipy.special.digamma, (-5, +5), (-5, +5), n)
-cplot.savefig("zeta.png", riemann_zeta, (-30, +30), (-30, +30), n)
-
-cplot.savefig("riemann-xi.png", riemann_xi, (-20, +20), (-20, +20), n)
-cplot.savefig("riemann-siegel-z.png", riemann_siegel_z, (-20, +20), (-20, +20), n)
-cplot.savefig(
-    "riemann-siegel-theta.png", riemann_siegel_theta, (-20, +20), (-20, +20), n
-)
-
-cplot.savefig("siam.png", siam, (-1, 1), (-1, 1), n, abs_scaling="h-0.5")
+cplot.plot(siam, (-1, 1), (-1, 1), n, abs_scaling="h-0.5")
+plt.savefig("siam.png", transparent=True, bbox_inches="tight")
+plt.close()
 
 
 def f(z):
@@ -119,10 +128,14 @@ def f(z):
 
 n = 201
 for name in ["cam16", "cielab", "oklab", "hsl"]:
-    cplot.savefig(f"{name}-10.png", f, (-3, +3), (-3, +3), n, colorspace=name)
-    cplot.savefig(
-        f"{name}-05.png", f, (-3, +3), (-3, +3), n, abs_scaling="h-0.5", colorspace=name
-    )
-    cplot.savefig(
-        f"{name}-00.png", f, (-3, +3), (-3, +3), n, abs_scaling="h-0", colorspace=name
-    )
+    cplot.plot(f, (-3, +3), (-3, +3), n, colorspace=name)
+    plt.savefig(f"{name}-10.png", transparent=True, bbox_inches="tight")
+    plt.close()
+    #
+    cplot.plot(f, (-3, +3), (-3, +3), n, colorspace=name, abs_scaling="h-0.5")
+    plt.savefig(f"{name}-10.png", transparent=True, bbox_inches="tight")
+    plt.close()
+    #
+    cplot.plot(f, (-3, +3), (-3, +3), n, colorspace=name, abs_scaling="h-0")
+    plt.savefig(f"{name}-10.png", transparent=True, bbox_inches="tight")
+    plt.close()
