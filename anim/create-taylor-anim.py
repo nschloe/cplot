22f4cbc62f90458314da1e30ea8b8d2868d81102
Nico Schlömer <nico.schloemer@gmail.com>
2021-12-06 17:35:05 +0100

    readme

--- anim/create-taylor-anim.py
+++ anim/create-taylor-anim.py
@@ -5,6 +5,7 @@ from typing import Callable
 import matplotlib.pyplot as plt
 import numpy as np
 import sympy
+from rich.progress import track
 from sympy import diff, lambdify, simplify
 
 import cplot
@@ -39,8 +40,7 @@ def create_taylor_anim(taylor: Taylor, p: cplot.Plotter, name: str, max_degree: 
     plt.close()
 
     idx = 0
-    for k in range(max_degree + 1):
-        print(f"{k}...")
+    for k in track(range(max_degree + 1), description="Creating PNGs..."):
         val = next(taylor)
         p.plot(val)
         plt.suptitle(f"Taylor expansion of {name} around {taylor.z0}, degree {k}")
@@ -49,25 +49,25 @@ def create_taylor_anim(taylor: Taylor, p: cplot.Plotter, name: str, max_degree: 
         idx += 1
 
 
-name = "exp"
-p = cplot.Plotter((-7.0, 7.0, 400), (-7.0, 7.0, 400))
-taylor = Taylor(sympy.exp, 0, p.Z)
-max_degree = 30
+# name = "exp"
+# p = cplot.Plotter((-7.0, 7.0, 400), (-7.0, 7.0, 400))
+# taylor = Taylor(sympy.exp, 0, p.Z)
+# max_degree = 30
 
 # name = "sin"
 # p = cplot.Plotter((-10.0, 10.0, 640), (-6.0, 6.0, 400))
-# taylor = Taylor(sympy.sin, 0.0, p.Z)
+# taylor = Taylor(sympy.sin, 0, p.Z)
 # max_degree = 30
 
 # name = "log"
 # p = cplot.Plotter((-0.7, 2.7, 400), (-1.7, 1.7, 400))
-# taylor = Taylor(sympy.log, 1.0, p.Z)
+# taylor = Taylor(sympy.log, 1, p.Z)
 # max_degree = 40
 
-# name = "tan"
-# p = cplot.Plotter((-2.2, 2.2, 400), (-2.2, 2.2, 400))
-# taylor = Taylor(sympy.tan, 0.0, p.Z)
-# max_degree = 40
+name = "tan"
+p = cplot.Plotter((-2.2, 2.2, 400), (-2.2, 2.2, 400))
+taylor = Taylor(sympy.tan, 0, p.Z)
+max_degree = 40
 
 
 create_taylor_anim(taylor, p, name, max_degree)
